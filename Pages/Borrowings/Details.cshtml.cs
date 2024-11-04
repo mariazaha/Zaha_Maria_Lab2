687f314bafaa43802a6c75c42f20736f7daab538
MARIA-VANESSA-BEATRICE ZAHA <maria.zaha@stud.ubbcluj.ro>
2024-11-04 12:07:42 +0200

    new scaffolded item borrowings

---
+++ Pages/Borrowings/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Zaha_Maria_Lab2.Data;
+using Zaha_Maria_Lab2.Models;
+
+namespace Zaha_Maria_Lab2.Pages.Borrowings
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Zaha_Maria_Lab2.Data.Zaha_Maria_Lab2Context _context;
+
+        public DetailsModel(Zaha_Maria_Lab2.Data.Zaha_Maria_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Borrowing Borrowing { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var borrowing = await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
+            if (borrowing == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Borrowing = borrowing;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/Borrowings/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Zaha_Maria_Lab2.Data;
+using Zaha_Maria_Lab2.Models;
+
+namespace Zaha_Maria_Lab2.Pages.Borrowings
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Zaha_Maria_Lab2.Data.Zaha_Maria_Lab2Context _context;
+
+        public IndexModel(Zaha_Maria_Lab2.Data.Zaha_Maria_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Borrowing> Borrowing { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Borrowing = await _context.Borrowing
+                .Include(b => b.Book)
+                .Include(b => b.Member).ToListAsync();
+        }
+    }
+}
