39c78b39b2a54a351023c7e7f16f7fd365714789
MARIA-VANESSA-BEATRICE ZAHA <maria.zaha@stud.ubbcluj.ro>
2024-11-10 16:34:18 +0200

    Creare cont, logare.

---
+++ Areas/Identity/Data/LibraryIdentityContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Zaha_Maria_Lab2.Models;
+
+namespace Zaha_Maria_Lab2.Data;
+
+public class LibraryIdentityContext : IdentityDbContext<IdentityUser>
+{
+    public LibraryIdentityContext(DbContextOptions<LibraryIdentityContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Member> Member { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Zaha_Maria_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Zaha_Maria_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Zaha_Maria_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Zaha_Maria_Lab2Context' not found.")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LibraryIdentityContext>();
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("Zaha_Maria_Lab2Context") ?? throw new InvalidOperationException("Connections tring 'Nume_Pren_Lab2Context' not found."))); 
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
