965236ca384443ebb73d6c20b0e763ba011ca878
MARIA-VANESSA-BEATRICE ZAHA <maria.zaha@stud.ubbcluj.ro>
2024-11-10 16:55:56 +0200

    Solved issue.

--- Program.cs
+++ Program.cs
@@ -9,15 +9,10 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Zaha_Maria_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Zaha_Maria_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Zaha_Maria_Lab2Context' not found.")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LibraryIdentityContext>();
 
-builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+builder.Services.AddDbContext<LibraryIdentityContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Zaha_Maria_Lab2Context") ?? throw new InvalidOperationException("Connections tring 'Nume_Pren_Lab2Context' not found."))); 
 
-options.UseSqlServer(builder.Configuration.GetConnectionString("Zaha_Maria_Lab2Context") ?? throw new InvalidOperationException("Connections tring 'Nume_Pren_Lab2Context' not found."))); 
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<LibraryIdentityContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
