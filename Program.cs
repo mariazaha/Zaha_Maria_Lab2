42f9c645db44e03a018f9699e211797c82102951
MARIA-VANESSA-BEATRICE ZAHA <maria.zaha@stud.ubbcluj.ro>
2024-11-15 23:19:03 +0200

    Accesul pentru paginile Create, Edit, Delete doar pentru Admin.

--- Program.cs
+++ Program.cs
@@ -5,14 +5,21 @@ using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Books");
+    options.Conventions.AllowAnonymousToPage("/Books/Index");
+    options.Conventions.AllowAnonymousToPage("/Books/Details");
+});
 builder.Services.AddDbContext<Zaha_Maria_Lab2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Zaha_Maria_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Zaha_Maria_Lab2Context' not found.")));
 
 
 builder.Services.AddDbContext<LibraryIdentityContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Zaha_Maria_Lab2Context") ?? throw new InvalidOperationException("Connections tring 'Nume_Pren_Lab2Context' not found."))); 
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options =>options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LibraryIdentityContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
